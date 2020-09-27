1d0f1aca8c9ff4d83a4b9c174ffb27a1b6b10135
Calix Huang <calix.huang1@gmail.com>
2020-09-26 21:58:57 -0700

    setup about

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 const index = () => {
   return (
     <div>
@@ -39,6 +43,33 @@ const index = () => {
                 <div className="tag">Tag</div>
                 <div className="tag">Tag</div>
                 <div className="tag">Tag</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="about-grid">
+            <img src="/img/people/calix.jpeg" alt="Calix Huang - Launch Tech LLC" />
+            <div className="about">
+              <h2>Calix Huang</h2>
+              <div className="tags my-2">
+                <div className="tag">Tag</div>
+                <div className="tag">Tag</div>
+                <div className="tag">Tag</div>
+                <div className="tag">Tag</div>
+                <div className="tag">Tag</div>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              </p>
+              <div className="social">
+                <a href="https://www.instagram.com/calixo888/" target="_blank"><FontAwesomeIcon icon={faInstagram}/></a>
+                <a href="https://www.linkedin.com/in/calix-huang/" target="_blank"><FontAwesomeIcon icon={faLinkedin}/></a>
+                <a href="https://github.com/calixo888" target="_blank"><FontAwesomeIcon icon={faGithub}/></a>
+                <a href="mailto:me@calix.dev" target="_blank"><FontAwesomeIcon icon={faEnvelope}/></a>
               </div>
             </div>
           </div>

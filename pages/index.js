502aa923b631d16bc1057ff45d28ec85385a8eab
Calix Huang <calix.huang1@gmail.com>
2020-09-26 21:25:59 -0700

    setup sass, header, and stats

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,30 @@ const index = () => {
   return (
     <div>
       <header>
-        <div className="container">
+        <div className="container text-center">
           <h1>Launch Tech LLC</h1>
+          <p>We build the world's best SaaS products!</p>
         </div>
       </header>
+
+      <section>
+        <div className="container">
+          <div className="stats-grid">
+            <div className="stat">
+              <span className="title">Interactions</span>
+              <span className="number">30,000</span>
+            </div>
+            <div className="stat">
+              <span className="title">Interactions</span>
+              <span className="number">30,000</span>
+            </div>
+            <div className="stat">
+              <span className="title">Interactions</span>
+              <span className="number">30,000</span>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
